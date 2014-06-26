41958f4f780c627f6151029cc967ca28c6bbde20
Daniel Arbiol <arbiol.daniel0727@gmail.com>
2014-06-26 16:56:33 +0800

    Modified Home.html

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$( document ).ready( function () {
+	$( '.dropdown-toggle' ).click(function() {
+		// $( '.dropdown-menu' ).slideToggle()
+		//$(this).next('.dropdown-menu').slideToggle(300);
+		$( '.nav-overlay' ).css( 'display', 'block' );
+		if( !$( this ).parent().hasClass( 'open' ) ) {
+			$( this ).parent().siblings('.dropdown').each( function () {
+				$( this ).children( 'a' ).next().css( 'display', 'none' );
+			} )
+		}
+		$( this ).next( '.dropdown-menu' ).slideToggle( 300 );
+	});
+
+	$( '.nav-overlay' ).click( function() {
+		$( '.nav-overlay' ).css( 'display', 'none' );
+		$( 'ul.dropdown-menu' ).css( 'display', 'none' );
+	} );
+
+} );
