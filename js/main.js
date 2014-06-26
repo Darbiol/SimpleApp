836383c555f76745cb85010c8b6515f6bd2fd41b
Daniel Arbiol <arbiol.daniel0727@gmail.com>
2014-06-26 23:08:21 +0800

    modified navbar to be responsive

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
 $( document ).ready( function () {
-	$( '.dropdown-toggle' ).click(function() {
-		// $( '.dropdown-menu' ).slideToggle()
-		//$(this).next('.dropdown-menu').slideToggle(300);
+
+	$( '.dropdown-toggle' ).click(function () {
+
 		$( '.nav-overlay' ).css( 'display', 'block' );
+
 		if( !$( this ).parent().hasClass( 'open' ) ) {
 			$( this ).parent().siblings('.dropdown').each( function () {
 				$( this ).children( 'a' ).next().css( 'display', 'none' );
 			} )
 		}
+
 		$( this ).next( '.dropdown-menu' ).slideToggle( 300 );
 	});
 
-	$( '.nav-overlay' ).click( function() {
+	$( '.nav-overlay' ).click( function () {
 		$( '.nav-overlay' ).css( 'display', 'none' );
 		$( 'ul.dropdown-menu' ).css( 'display', 'none' );
+	} );
+
+	$( window ).resize( function () {
+		//alert( 'window has resized' )
 	} );
 
 } );
